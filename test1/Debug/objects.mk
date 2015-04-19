89b0d1f49bede70b1548cf396decaef39ac09670
Rohit Arora <rohitarora.contact@gmail.com>
2015-04-19 14:45:55 -0400

    Delete Unwanted Test Folder

--- test1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lfuse -lmemstream -ldropbox -lcrypto -lssl -lglib-2.0 -lfuse -lglib-2.0
-
--- test1/Debug/test2.d
+++
@@ -1,1 +0,0 @@
-test2.d: ../test2.c
