3de2f26d2e2c7ea36627ac14d40b53498103ac96
Rohit Arora <rohitarora.contact@gmail.com>
2015-04-19 19:58:11 -0400

    New Division

---
+++ dropbox.c
@@ -0,0 +1,366 @@
+#include "storage.h"
+
+/************************Dropbox API code*********************/
+drbClient * intialize_drop_box();
+void move_to_dropbox_cold_storage(char * );
+void fetch_from_dropbox_cold_storage(char * );
+void upload_dropbox_file(char *, char *, char *);
+int download_dropbox_file(char *, char *, char *);
+int connect_to_dropbox(drbClient* , char *, char *);
+void write_access_cold_blocks(Node);
+void print_cold_blocks(Block);
+char * hash_calc(Block);
+void iterator(gpointer, gpointer, gpointer);
+void create_hashtree();
+void activate_hashtree();
+gboolean hashtree_contains(char *);
+void write_block_to_file(Block, char * );
+void update_hashtree(char *);
+void read_block_from_file(char *, Block);
+void read_access_cold_blocks(Node);
+
+
+drbClient * intialize_drop_box(){
+
+	char *c_key    = "e9kwa4b6lnxk0r5";  //< consumer key
+	char *c_secret = "sr8lt6xj5izjtol";  //< consumer secret
+	//char *t_key    = "KSkQCyhUYNIAAAAAAAAACdaOg1cDsSD4GBI_Q_I7ajkC5zhzBCZnk00rtMrfmhbf"; //< access token key
+	char *t_key = "2vrs94p0v2evy90a";
+	char *t_secret = "b33n459lg42bzom";  //< access token secret
+	drbInit();
+	drbClient* cli_local = drbCreateClient(c_key, c_secret, t_key, t_secret);
+	connect_to_dropbox(cli_local, t_key, t_secret);
+	drbSetDefault(cli_local, DRBOPT_ROOT, DRBVAL_ROOT_AUTO, DRBOPT_END);
+
+	return cli_local;
+}
+
+void move_to_dropbox_cold_storage(char * fileName){
+
+	char DRBFilePath[255];
+	char path[255];
+
+	strcpy(path,"./");
+	strcat(path, fileName);
+	strcpy(DRBFilePath, "/");
+	strcat(DRBFilePath, fileName);
+
+	//printf("File name: %s \n", fileName);
+	//printf("Local file path: %s \n", path);
+	//printf("Dropbox file path: %s \n", DRBFilePath);
+
+	upload_dropbox_file(path, fileName, DRBFilePath);
+}
+
+void fetch_from_dropbox_cold_storage(char * fileName){
+
+	char DRBFilePath[255];
+	char path[255];
+
+	strcpy(path,"./");
+	strcat(path, fileName);
+	strcpy(DRBFilePath, "/");
+	strcat(DRBFilePath, fileName);
+
+	download_dropbox_file(path, fileName, DRBFilePath);
+}
+
+void upload_dropbox_file(char *localFilePath, char *fileName, char *DRBFilePath){
+
+	int err;
+	FILE *file = fopen(localFilePath, "r");
+	printf("Uploading File %s...\n", fileName);
+	err = drbPutFile(cli, NULL,
+			DRBOPT_PATH, DRBFilePath,
+			DRBOPT_IO_DATA, file,
+			DRBOPT_IO_FUNC, fread,
+			DRBOPT_END);
+	fclose(file);
+	printf("File upload: %s\n", err ? "Failed" : "Successful");
+	remove(localFilePath);
+}
+
+int download_dropbox_file(char *localFilePath, char *fileName, char *DRBFilePath){
+
+	int err;
+	FILE *file = fopen(localFilePath, "w"); // Write it in this file
+	void* output = NULL;
+	printf("\nDownloading File %s...\n", fileName);
+	err = drbGetFile(cli, &output,
+			DRBOPT_PATH, DRBFilePath,
+			DRBOPT_IO_DATA, file,
+			DRBOPT_IO_FUNC, fwrite,
+			DRBOPT_END);
+	fclose(file);
+	printf("File Download: %d\n", err );
+	return err;
+}
+
+int connect_to_dropbox(drbClient* cli_local, char *t_key, char *t_secret){
+	// Request a AccessToken if undefined (NULL)
+	if(!t_key || !t_secret) {
+		drbOAuthToken* reqTok = drbObtainRequestToken(cli_local);
+
+		if (reqTok) {
+			char* url = drbBuildAuthorizeUrl(reqTok);
+			printf("Please visit %s\nThen press Enter...\n", url);
+			free(url);
+			fgetc(stdin);
+
+			drbOAuthToken* accTok = drbObtainAccessToken(cli_local);
+
+			if (accTok) {
+				// This key and secret can replace the NULL value in t_key and
+				// t_secret for the next time.
+				printf("key:    %s\nsecret: %s\n", accTok->key, accTok->secret);
+			} else{
+				fprintf(stderr, "Failed to obtain an AccessToken...\n");
+				return EXIT_FAILURE;
+			}
+		} else {
+			fprintf(stderr, "Failed to obtain a RequestToken...\n");
+			return EXIT_FAILURE;
+		}
+	}
+	return 0;
+}
+/*************************************************************/
+
+//***********************WRITE TO COLD STORAGE****************/
+
+/************************Code for Accessing Blocks**************/
+/*void check_all_hashes(Node cold_file){
+	Block temp = cold_file->data;
+
+	char *current_hash;
+
+	while(temp != NULL)
+	{
+		current_hash = temp->server_block_hash;
+		printf("\n\nHash under consideration: %s\n\n",current_hash);
+		temp = temp->nxt_blk;
+	}
+}*/
+/************************Code for Accessing Blocks**************/
+void write_access_cold_blocks(Node cold_file){
+
+	Block temp = cold_file->data;
+	char *hash;
+
+	while(temp != NULL)
+	{
+		if(difftime(latest_file_access_time,cold_file->access_time) >= 0){
+			hash = hash_calc(temp);
+
+			if(!hashtree_contains(hash)){
+				write_block_to_file(temp,hash);
+				move_to_dropbox_cold_storage(hash);
+				update_hashtree(hash);
+			}else
+			{
+				printf("Block Already on Server.\n");
+			}
+
+			temp->server_block_hash = hash;
+			temp->inmemory_flag = False;
+
+			//print_cold_blocks(temp);
+			temp = temp->nxt_blk;
+		}
+		else{
+			printf("Cold File %s accessed just now! Breaking File Transfer. Cold File Access Time: %ld, Benchmark Time: %ld\n",cold_file->name,cold_file->access_time,latest_file_access_time);
+			return;
+		}
+	}
+
+	// Freeing all the transfered blocks for host storage.
+	pthread_mutex_lock(&(cold_file->lock));
+	temp = cold_file->data;
+
+	while(temp != NULL){
+		printf("Freeing Block\n");
+		free_blk[temp->blk_num] = -1;
+		memset(memory_blocks[temp->blk_num], '\0',BLOCK_SIZE);
+		pthread_mutex_lock(&count_lock);
+		free_block_count++;
+		pthread_mutex_unlock(&count_lock);
+		temp = temp->nxt_blk;
+	}
+
+	cold_file->inmemory_node_flag=False;
+	
+	pthread_mutex_unlock(&(cold_file->lock));	
+	//check_all_hashes(cold_file); 
+}
+
+void print_cold_blocks(Block temp){
+	printf("\nAfter Changing Data Block \"%d\" and data: \"%s\" \n",temp->blk_num, temp->server_block_hash);
+}
+
+/***********************Code for Finding Hash for a Block***************/
+char * hash_calc(Block cold_block)
+{
+	int i;
+	char *buf;
+	buf = (char *) malloc(sizeof(char)*SHA_DIGEST_LENGTH*2 + 1);
+	unsigned char hash[SHA_DIGEST_LENGTH];
+
+	memset(buf, 0x0,SHA_DIGEST_LENGTH*2);
+
+	SHA1(memory_blocks[cold_block->blk_num], BLOCK_SIZE, hash);
+
+	for (i = 0; i < SHA_DIGEST_LENGTH; i++) {
+		sprintf((char*)&(buf[i*2]), "%02x", hash[i]);
+	}
+	buf[SHA_DIGEST_LENGTH*2]='\0';
+	return buf;
+}
+
+/***********************Code for Getting Server Side Hashtree (hashOfBlock -> BlockNames)***************/
+void iterator(gpointer key, gpointer value, gpointer user_data) {
+	printf(user_data, key, value);
+}
+
+void create_hashtree(){
+
+	char hash[40];
+	char block_name[20];
+	char linebuff[61];
+	hashtree = g_hash_table_new(g_str_hash, g_str_equal);
+	block_number = 0;
+
+	FILE *hash_file;
+	hash_file= fopen("./dropbox_hashtree.txt", "r");
+
+	while(fgets(linebuff, sizeof(linebuff), hash_file)){
+		sscanf(linebuff,"%s %s",hash,block_name);
+		g_hash_table_insert(hashtree,  g_strdup(hash),  g_strdup(block_name));
+
+		block_number++;
+	}
+
+	fclose(hash_file);
+}
+
+void activate_hashtree(){
+
+	int hashtree_file_exist = download_dropbox_file("./dropbox_hashtree.txt", "dropbox_hashtree.txt", "/dropbox_hashtree.txt");
+
+	if(hashtree_file_exist == 0){
+		create_hashtree();
+	}
+	else{
+		hashtree = g_hash_table_new(g_str_hash, g_str_equal);
+	}
+}
+
+/***********************Code for Identifying duplicate block in Server Side Hashtree***************/
+gboolean hashtree_contains(char *hash){
+
+	gboolean contains = g_hash_table_contains(hashtree, hash);
+
+	return contains;
+}
+
+/***********************Code for Changing each non-duplicate block to a file (block_1, block_2)***************/
+void write_block_to_file(Block cold_block, char * file_name){
+
+	FILE *fp;
+	int file_write_result;
+
+	char temp_path[80];
+        memset(temp_path,'\0',80);
+	strcpy(temp_path, "./");
+	strcat(temp_path, file_name);
+
+	fp=fopen(temp_path, "wb");
+	//printf("Read Content: %s", memory_blocks[cold_block->blk_num]);
+	file_write_result = fwrite(memory_blocks[cold_block->blk_num],BLOCK_SIZE,1,fp);
+	//file_write_result = fprintf(fp,"%s", memory_blocks[cold_block->blk_num]);
+	printf("\nFile Write Result : %d", file_write_result);
+
+	fclose(fp);
+}
+
+/***********************Code for updating server side Hashtree***************/
+void update_hashtree(char *hash){
+
+	char block_name[20];
+	sprintf(block_name, "block_%d", block_number);
+	block_number++;
+
+	g_hash_table_insert(hashtree, hash, block_name);
+
+	FILE * file= fopen("./dropbox_hashtree.txt", "a");
+	if (file != NULL) {
+		fprintf(file, "%s %s\n", hash, block_name);
+		fclose(file);
+	}
+}
+
+/***********************Code for Updating client Side Hashtree of Blocks(File -> hashofBlocks): Not Required***************/
+
+/***********************READ FROM COLD STORAGE***************/
+/***********************Code for Using client Side Hashtree of Blocks(File -> hashofBlocks)***************/
+/***********************Code for Getting Server Side Hashtree (hashOfBlock -> BlockNames: Already Implemented Above)***************/
+/***********************Code for Identifying Block Files to be copied from Server***************/
+/***********************Code for Writing Block File data to blocks on client***************/
+void read_block_from_file(char * file_name, Block temp){
+
+	FILE *fp;
+	char temp_path[80];
+	char *string = malloc(BLOCK_SIZE + 1);
+
+	strcpy(temp_path, "./");
+	strcat(temp_path, file_name);
+
+	fp=fopen(temp_path, "rb");
+	fread(string, BLOCK_SIZE, 1, fp);
+
+	fclose(fp);
+	//printf("Fetched String: %s",string);
+
+	long fblk = getFreeBlock();
+	temp -> blk_num =fblk;
+	strncpy(memory_blocks[fblk], string, strlen(string));
+
+	remove(file_name);
+}
+
+void read_access_cold_blocks(Node cold_file){
+
+	Block temp = cold_file->data;
+	char *hash;
+	int hashtree_activated = 0;
+
+	/**********Move this to calling function*********************/
+	if(thread_flag != 1){
+		activate_hashtree();
+		hashtree_activated = 1;
+	}
+
+	while(temp != NULL)
+	{
+		hash = temp->server_block_hash;
+
+		if(hashtree_contains(hash)){
+			fetch_from_dropbox_cold_storage(hash);
+			read_block_from_file(hash, temp);
+		}else
+		{
+			printf("Block not found on Server.\n");
+		}
+
+		temp->server_block_hash = NULL;
+		temp->inmemory_flag = True;
+
+		//print_cold_blocks(temp);
+		temp = temp->nxt_blk;
+	}
+	/**********Move this to calling function*********************/
+	cold_file->inmemory_node_flag = True;
+	if(hashtree_activated == 1){
+		remove("./dropbox_hashtree.txt");
+	}
+}
+/***********************Code for for Updating client Side Hashtree of Blocks(File -> hashofBlocks)***************/
--- storage.h
+++ storage.h
@@ -1,8 +1,29 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <openssl/sha.h>
+#include <dropbox.h>
+#include <memStream.h>
+#include <glib.h> 
+#include "fuse_common.h"
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define FUSE_USE_VERSION 26
+#define FILENAME_SIZE 30
+#define FULLPATHNAME 1000 
+#define BLOCK_SIZE 4096
+#define MB_CONVERT 1024*1024
+#define MAX_STORAGE_THRESHOLD 70
+#define OPTIMAL_STORAGE_THRESHOLD 50
+#define MIN_STORAGE_THRESHOLD 30
 
 typedef enum {Nfile, Ndir} Ntype;
 
@@ -39,9 +60,68 @@ typedef struct List_item {
 	struct List_item *next;
 }List_item ;
 
-List_item *acclist_head = NULL;
-List_item *transfer_list = NULL;
-List_item *rtvlist_head = NULL;
+// List for access and retrival of files.
+extern List_item *acclist_head;
+extern List_item *transfer_list;
+extern List_item *rtvlist_head;
 //List_item *fs_head = NULL;
+
+// Flags
+extern int thread_flag;
+extern time_t latest_file_access_time;
+
+// Locks
+extern pthread_mutex_t count_lock;
+
+// Array maintaining free blocks
+extern int *free_blk;
+
+// defining root node here 
+extern Node root,buffNode;
+extern char **memory_blocks;
+// As there is input limit for the memory defining global variables 
+
+// Memory Counter variables
+extern long long malloc_counter,malloc_limit,block_count, free_block_count;
+extern int block_number;
+
+// Dropboc Client and HashTree Data Structure
+extern drbClient *cli;
+extern GHashTable* hashtree; 
+
+drbClient * intialize_drop_box();
+void move_to_dropbox_cold_storage(char * );
+void fetch_from_dropbox_cold_storage(char * );
+void upload_dropbox_file(char *, char *, char *);
+int download_dropbox_file(char *, char *, char *);
+int connect_to_dropbox(drbClient* , char *, char *);
+void write_access_cold_blocks(Node);
+void print_cold_blocks(Block);
+char * hash_calc(Block);
+void iterator(gpointer, gpointer, gpointer);
+void create_hashtree();
+void activate_hashtree();
+gboolean hashtree_contains(char *);
+void write_block_to_file(Block, char * );
+void update_hashtree(char *);
+void read_block_from_file(char *, Block);
+void read_access_cold_blocks(Node);
+
+int checkStorageThreshold(int);
+void insert_back(Node, List_item **);
+Node get_inode(List_item **);
+List_item *sort_list(List_item *);
+List_item *sort_list_size(List_item *);
+void print_list(List_item *);
+int populate_access_list();
+long calc_blcks_transfer(int);
+int calc_file_blks(int);
+void prepare_nodelist_to_transfer();
+void *track_cold_files();
+int checkMinStorageThreshold(int);
+int populate_retrieval_list();
+void print_retrieval_list();
+List_item *sort_rlist(List_item *);
+void *get_cold_files();
 
 #endif
